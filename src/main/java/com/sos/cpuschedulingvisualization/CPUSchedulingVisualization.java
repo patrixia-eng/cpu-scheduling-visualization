63f8a8f086017916ef9d6b7d53dcb09b500caa3e
Patrixia Hernando <pantinehernando20005@gmail.com>
2025-07-12 11:22:58 +0800

    Initial commit -  Java project setup

---
+++ src/main/java/com/sos/cpuschedulingvisualization/CPUSchedulingVisualization.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sos.cpuschedulingvisualization;
+
+/**
+ *
+ * @author Nitro-5
+ */
+public class CPUSchedulingVisualization {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
